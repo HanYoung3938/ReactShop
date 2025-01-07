ac2023ea7a4cc06dcd46eb6cc032d65c94fb2962
박한영 <poungharkyan524@gmail.com>
2025-01-07 09:16:07 +0900

    킹받는 김율

---
+++ src/data.js
@@ -0,0 +1,28 @@
+// let array = [Park, 19]
+// let 오브젝트 = {name: 'Park', age: 19}
+
+
+let data = [
+    {
+    id : 0,
+    title : "White and Blue",
+    content : "Born in France",
+    price : 120000
+    },
+    
+    {
+    id : 1,
+    title : "Gray Knit",
+    content : "Born in Seoul",
+    price : 110000
+    },
+    
+    {
+    id : 2,
+    title : "Black Yordan",
+    content : "Born in the States",
+    price : 130000
+    }
+    ];
+
+    export default data;
